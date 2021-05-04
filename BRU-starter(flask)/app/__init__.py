fa6b9693cb0f8488644c62d500366764db497335
Bergan Oudshoorn <Bergan00@gmail.com>
2021-05-04 14:15:56 -0400

    databases DONE

--- BRU-starter(flask)/app/__init__.py
+++ BRU-starter(flask)/app/__init__.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
-from .models import db, User
+from .models import db
+from .models.user import User, Post, Category, Review, Reply, Discussion
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 
--- BRU-starter(flask)/app/models/user.py
+++ BRU-starter(flask)/app/models/user.py
@@ -2,6 +2,20 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+# ---------------Joint Table----------------
+# when creating a joint table it is highly recommended to go about it by creating the table
+# like this rather than with a class like any other normal table
+shopping_cart = db.Table(
+    'shopping_carts',
+    db.Column(
+        "user_id", db.Integer, db.ForeignKey('users.id'), nullable=False
+    ),
+    db.Column(
+        "post_id", db.Integer, db.ForeignKey('posts.id'), nullable=False
+    )
+)
+
+# ------------------------------Users Table ----------------------------------
 class User(db.Model, UserMixin):
   __tablename__ = 'users'
 
@@ -11,6 +25,13 @@ class User(db.Model, UserMixin):
   hashed_password = db.Column(db.String(255), nullable = False)
   image = db.Column(db.String(255))
 
+  #Relations
+  post_creator = db.relationship('Post', back_populates="creator")
+  discussion_creator = db.relationship('Discussion', back_populates='discussion')
+  user_review = db.relationship('Review', back_populates="creator_review")
+  creator_reply = db.relationship('Reply', back_populates="user_reply")
+
+  posts = db.relationship("Post", secondary=shopping_cart, back_populates="users", lazy="dynamic")
 
   @property
   def password(self):
@@ -30,7 +51,7 @@ class User(db.Model, UserMixin):
     return {
       "id": self.id,
       "username": self.username,
-      "email": self.email
+      "email": self.email,
       "image": self.image
     }
 
@@ -43,15 +64,22 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_title = db.Column(db.String(100), nullable= False)
     image = db.Column(db.String(255))
-    description = db.Column(db.TextField, nullable= False)
+    description = db.Column(db.Text, nullable= False)
     year = db.Column(db.String(20))
-    model = db.Column(db.Srting(20))
+    model = db.Column(db.String(20))
     price = db.Column(db.String(50))
     in_stock = db.Column(db.String(20))
-    new_used = db.Column(db.BooleanField, nullable=False)
+    new_used = db.Column(db.Boolean, nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     categoryId = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable= False)
     created_at = db.Column(db.Date)
+
+    #Relations
+    creator = db.relationship('User', back_populates="post_creator")
+    category = db.relationship('Category', back_populates="post_category")
+    user_post = db.relationship('Review', back_populates="creator_post")
+
+    users = db.relationship("User", secondary=shopping_cart, back_populates="posts", lazy="dynamic")
 
 
     def to_dict(self):
@@ -68,20 +96,22 @@ class Post(db.Model):
             "userId": self.userId,
             "categoryId": self.categoryId,
             "created_at": self.created_at
-
         }
 
 # ------------------------------Discussions Table ----------------------------------
-
 class Discussion(db.Model):
     __tablename__ = 'discussions'
 
     id = db.Column(db.Integer, primary_key=True)
     discussion_title = db.Column(db.String(100), nullable= False)
-    body = db.Column(db.TextField)
+    body = db.Column(db.Text)
     image = db.Column(db.String(255))
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.Date)
+
+    #Relations
+    discussion = db.relationship('User', back_populates="discussion_creator")
+    discussion_post = db.relationship('Reply', back_populates='user_discussion')
 
 
     def to_dict(self):
@@ -92,6 +122,71 @@ class Discussion(db.Model):
             "image": self.image,
             "userId": self.userId,
             "created_at": self.created_at
+        }
+
+# ------------------------------Reviews Table ----------------------------------
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    postId = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable= False)
+    body = db.Column(db.Text)
+    created_at = db.Column(db.Date)
+
+    #Relations
+    creator_review = db.relationship('User', back_populates="user_review")
+    creator_post = db.relationship('Post', back_populates="user_post")
 
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.userId,
+            "postId": self.postId,
+            "body": self.body,
+            "created_at": self.created_at
+        }
+
+# ------------------------------Replies Table ----------------------------------
+class Reply(db.Model):
+    __tablename__ = 'replies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    discussionId = db.Column(db.Integer, db.ForeignKey('discussions.id'), nullable= False)
+    body = db.Column(db.Text)
+    created_at = db.Column(db.Date)
+
+    #Relations
+    user_reply = db.relationship('User', back_populates="creator_reply")
+    user_discussion = db.relationship('Discussion', back_populates="discussion_post")
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.userId,
+            "discussionId": self.discussionId,
+            "body": self.body,
+            "created_at": self.created_at
+        }
+
+# ------------------------------Categories Table ----------------------------------
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100))
+    created_at = db.Column(db.Date)
+
+    #Relations
+    post_category = db.relationship('Post', back_populates='category')
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "category": self.category,
+            "created_at": self.created_at
         }
