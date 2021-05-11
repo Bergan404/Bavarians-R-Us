a14b527dfb7a56f3d6b19dfcb7e074ad047786e2
Bergan Oudshoorn <Bergan00@gmail.com>
2021-05-11 14:19:16 -0400

    fixing

--- BRU-starter(flask)/app/seeds/posts.py
+++ BRU-starter(flask)/app/seeds/posts.py
@@ -45,6 +45,56 @@ def seed_posts():
                        description="This is a VRSF Racing downpipe that I had purchased but then ended up crashing my car in a lake.",
                        year="2015", model='228i', price='$235', in_stock='yes', new_used=True, userId=2, categoryId=2, created_at=datetime.now())
 
+    fifth_post = Post(post_title="BC Racing Coilovers",
+                       image='https://cdn.shopify.com/s/files/1/0941/4966/products/I-03-BR-BMW-E90-BC-Coilovers-1_98015ec4-db76-4b88-a711-fadbfc5ffcfc_1024x1024.jpg?v=1581716669',
+                       description="I have had these for little over a year and got them as a presesnt from my grandma, don't need them because I already have them!",
+                       year="2008", model='328i', price='$1100', in_stock='yes', new_used=True, userId=52, categoryId=4, created_at=datetime.now())
+
+    sixth_post = Post(post_title="M4 Coilovers",
+                       image='https://cdn.shopify.com/s/files/1/0941/4966/products/I-68-BR-BMW-F80-BC-racing-coilovers-1_1024x1024.jpg?v=1581717048',
+                       description="These are top of the line BC Racing Coilovers and they do have some were and tear on them, but you have the ability to lower the car at least up to 2 inches.",
+                       year="15-17", model='F82/F80', price='$856', in_stock='yes', new_used=True, userId=43, categoryId=4, created_at=datetime.now())
+
+    seventh_post = Post(post_title="Bmw Ecu",
+                       image='https://www.the-ecu-pro.com/wp-content/uploads/products/Professional-1999-E46-BMW-330d--EDC15-ECU-repair-service-scaled.jpg',
+                       description="This is from my parts car and I still works so don't get to discouraged",
+                       year="1999", model='E46', price='$399', in_stock='yes', new_used=True, userId=51, categoryId=7, created_at=datetime.now())
+
+    eigth_post = Post(post_title="BMW cobb accessport",
+                       image='https://media.cobbtuning.com/catalog/products/accessport_v3_bmw_001.jpg',
+                       description="This is for a N54 motor and only for that This can totally get your cars in the 3s",
+                       year="06-10", model='E90', price='$650', in_stock='yes', new_used=True, userId=33, categoryId=7, created_at=datetime.now())
+
+    ninth_post = Post(post_title="E30 5 lug conversion kit",
+                       image='https://i1.wp.com/srs-concept.com/wp-content/uploads/2016/11/5-lug-swap-complete-kit-render.png?fit=1833%2C929&ssl=1',
+                       description="This is for the bmw E30 and it is meant for switching that old 4 lug to a 5 lug",
+                       year="1987", model='E30', price='$470.96', in_stock='yes', new_used=True, userId=2, categoryId=6, created_at=datetime.now())
+
+    tenth_post = Post(post_title="Drilled and Slotted Rotors",
+                       image='https://m.media-amazon.com/images/I/81bsC9JSQXL._AC_.jpg',
+                       description="For the 335i wont fit any other car the ratio will be to big for the calipher",
+                       year="06-10", model='E90 335i', price='$200', in_stock='yes', new_used=True, userId=51, categoryId=6, created_at=datetime.now())
+
+    eleventh_post = Post(post_title="Bmw Clutch kit",
+                       image='https://www.fcpeuro.com/public/assets/products/157137/large/K70238-01-1.jpg?1496436679',
+                       description="Brand new clutch kit for the bwm m3",
+                       year="2010", model='M3', price='$286.49', in_stock='yes', new_used=True, userId=35, categoryId=3, created_at=datetime.now())
+
+    twelth_post = Post(post_title="Armytrix Exhaust M4",
+                       image='https://www.armytrix.com/cdn/800_530_100_ff_cdn/product-bmw-m4-w-gold-tips/bmw-m4-f82-armytrix-exhaust-08.jpg',
+                       description="This thing came off of my m4 and it sounds very much quite nice",
+                       year="15-16", model='M4 F82', price='$899.99', in_stock='yes', new_used=True, userId=52, categoryId=2, created_at=datetime.now())
+
+    thirteen_post = Post(post_title="BMW E60 Transmission",
+                       image='https://i.ebayimg.com/images/g/tLUAAOSwlzRabrVZ/s-l300.jpg',
+                       description="I have had this laying around for a while and now I gotta rid myself of it",
+                       year="04-05", model='530i/525i', price='$50', in_stock='yes', new_used=True, userId=23, categoryId=3, created_at=datetime.now())
+
+    fourteen_post = Post(post_title="Valve Cover",
+                       image='https://images-na.ssl-images-amazon.com/images/I/51J5moTX0%2BL._AC_SY355_.jpg',
+                       description="I know these be going out on everyones cars after a few thousand miles or so.",
+                       year="2008", model='E90', price='$150', in_stock='yes', new_used=True, userId=22, categoryId=1, created_at=datetime.now())
+
     db.session.add(first_post)
 
     db.session.add(second_post)
@@ -52,6 +102,27 @@ def seed_posts():
     db.session.add(third_post)
 
     db.session.add(fourth_post)
+
+    db.session.add(fifth_post)
+
+    db.session.add(sixth_post)
+
+    db.session.add(seventh_post)
+
+    db.session.add(eigth_post)
+
+    db.session.add(ninth_post)
+
+    db.session.add(tenth_post)
+
+    db.session.add(eleventh_post)
+
+    db.session.add(twelth_post)
+
+    db.session.add(thirteen_post)
+
+    db.session.add(fourteen_post)
+
 
     db.session.commit()
 
